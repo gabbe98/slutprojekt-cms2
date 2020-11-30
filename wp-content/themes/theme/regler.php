eb85dee84bea5b98024ddec271c998119ed79c9b
puttzon <puttzon@gmail.com>
2020-11-30 10:18:08 +0100

    Skapat mall för regler och villkor

---
+++ wp-content/themes/theme/regler.php
@@ -0,0 +1,22 @@
+<?php
+/*
+*Template name: Regler och villkor
+ */
+wp_head()
+?>
+
+<?php
+while (have_posts()) {
+  the_post();
+?>
+<?php } ?>
+
+<section>
+  <div class="container w-75 mt-3 mb-3">
+    <h1 class="mb-3"> <?php the_title() ?> </h1>
+    <p> <?php the_content() ?> </p>
+  </div>
+</section>
+
+
+<?php wp_footer() ?>
