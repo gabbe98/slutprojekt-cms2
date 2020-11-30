2e3cfa046646c9d4144af6169abdd20fdf35c6b0
puttzon <puttzon@gmail.com>
2020-11-30 14:16:12 +0100

    Tog bort kod på senaste commiten. Skall göras om

--- wp-content/themes/theme/varabutiker.php
+++ wp-content/themes/theme/varabutiker.php
@@ -2,27 +2,5 @@
 
 /**
  * Template name: Våra butiker
+ * SKall eventuellt tas bort
  */
-get_header();
-require 'varabutiker-acf.php';
-?>
-
-<?php
-while (have_rows('butiker')) {
-  the_row();
-?>
-<?php } ?>
-
-
-<div class="container w-75 mt-3">
-  <h1 class="mt-3"> <?php the_title(); ?> </h1>
-  <h3><?php the_field('butiksnamn1'); ?></h3>
-  <h5><?php the_field('oppetider1'); ?></h5>
-  <img src=" <?php echo get_field('karta1')['url']; ?>" alt="" width="250" height="250">
-  <h3><?php the_field('butiksnamn2'); ?></h3>
-  <h5><?php the_field('oppetider2'); ?></h5>
-  <img src=" <?php echo get_field('karta1')['url']; ?>" alt="" width="250" height="250">
-
-</div>
-
-<?php get_footer(); ?>
