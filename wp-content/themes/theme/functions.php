43542713cbdc8ecb846d6822fc7c5edc822bf4c9
puttzon <puttzon@gmail.com>
2021-01-15 12:32:02 +0100

    Lagt till deletefuntion av transient för butiker

--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -5,16 +5,17 @@ function slutprojekt_enqueue_scripts()
     wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), null, 'all');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), null, 'all');
 
-    wp_enqueue_script( 'jQuery', 'https://code.jquery.com/jquery-3.5.1.min.js', array(), null, true );
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), null, true );
+    wp_enqueue_script('jQuery', 'https://code.jquery.com/jquery-3.5.1.min.js', array(), null, true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'slutprojekt_enqueue_scripts');
 
 // Funktion för att ändra "läs mer" på excerpt till länk för inlägget
-function wpdocs_excerpt_more( $more ) {
+function wpdocs_excerpt_more($more)
+{
     return '<a href="' . get_the_permalink() . '">...</a>';
 }
-add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
+add_filter('excerpt_more', 'wpdocs_excerpt_more');
 
 
 //Funktion för custom-post butiker, Putte
@@ -41,9 +42,10 @@ function butiker()
 add_action('init', 'butiker');
 
 // Woocommerce stöd
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-    add_theme_support( 'woocommerce' );
+add_action('after_setup_theme', 'woocommerce_support');
+function woocommerce_support()
+{
+    add_theme_support('woocommerce');
 }
 
 
@@ -52,32 +54,43 @@ function woocommerce_support() {
 // ==================================
 
 // Registrera ett eget meny alternativ
-function register_nav_bar() {
-register_nav_menu('slutprojekt_nav_bar', __( 'Nav Bar'));
+function register_nav_bar()
+{
+    register_nav_menu('slutprojekt_nav_bar', __('Nav Bar'));
 }
-add_action( 'init', 'register_nav_bar' );
+add_action('init', 'register_nav_bar');
 
 // Lägg
-function add_nav_item_class( $classes, $args ) {
-    if ( 'slutprojekt_nav_bar' === $args->theme_location ) {
+function add_nav_item_class($classes, $args)
+{
+    if ('slutprojekt_nav_bar' === $args->theme_location) {
         $classes[] = 'nav-item';
     }
-    
+
     return $classes;
 }
-add_filter( 'nav_menu_css_class', 'add_nav_item_class', 10, 3 );
+add_filter('nav_menu_css_class', 'add_nav_item_class', 10, 3);
 
-function add_link_attributes($attributes) {
+function add_link_attributes($attributes)
+{
     $attributes['class'] = "nav-link";
     return $attributes;
 }
-add_filter( 'nav_menu_link_attributes', 'add_link_attributes');
+add_filter('nav_menu_link_attributes', 'add_link_attributes');
 
 //Stöd för google maps i ACF
 
-function my_acf_google_map_api( $api ){
+function my_acf_google_map_api($api)
+{
     $api['key'] = 'AIzaSyDXYJDOsJU6QmvuFi-D4r3w1yaWRPVahmE';
     return $api;
 }
 
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+//Tar bort transient om man lägger till ny butik, Putte
+add_action('save_post', function () {
+    if (false !== get_transient('varabutiker')) {
+        delete_transient('varabutiker');
+    }
+});
